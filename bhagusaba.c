41539e4c319e8b694d8457d6074f8e5610df9214
ISHIKA RAJANI <ishikarajani21@gmail.com>
2024-08-27 11:38:53 +1000

    first commit

---
+++ bhagusaba.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float main()
+{
+    float a,b;
+    printf("enter your first value");
+    scanf("%f",&a);
+    printf("enter your second value");
+    scanf("%f",&b);
+    printf("your addition is %f",a+b);
+    printf("your subtraction is %f",a-b);
+    printf("your multiplication is %f",a*b);
+    printf("your devision is %f",a/b);
+}
